4e9919d3a765d81484c4c0ded60ad09c63e4d18b
Cedric Hermans <hermans.cedric94@gmail.com>
2021-01-03 18:06:03 +0100

    chore: created an Item object

---
+++ item.js
@@ -0,0 +1,9 @@
+class Item {
+    constructor(name, description, deadline, state) {
+        this.name = name;
+        this.description = description;
+        this.startDate = new Date();
+        this.deadline = deadline;
+        this.state = state;
+    }
+}
