87b45e6ea55b21bbbb2b92b1b52f430458783583
Cedric Hermans <hermans.cedric94@gmail.com>
2021-01-07 21:19:20 +0100

    chore: add event listener to submit button

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const ipcRenderer = require('electron').ipcRenderer
 
 var item = document.getElementById('item');
 
+var form = document.getElementById('itemForm');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("added item");
+})
+
 ipcRenderer.on('items', (event, items) => {
     var response = JSON.parse(items);
     for (var i = 0; i < response.items.length; i++) {
